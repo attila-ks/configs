0aa8647d2a786129468ec0a58624efa996543981
attila-ks <attila.ks@protonmail.com>
2025-11-08 15:56:48 +0100

    Fix Zed error: Failed to load environment variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    if not set -q TMUX
-        exec tmux
-    end
+    # if not set -q TMUX
+    #    exec tmux
+    #end
 
     zoxide init --cmd j fish | source
     starship init fish | source
